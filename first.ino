53fe75fd990ecf5260ba0e0e7b5f125a3bdc2bb6
r13 <rudrobhowmik13@gmail.com>
2026-07-23 18:49:44 +0600

    second edit

--- first.ino
+++ first.ino
@@ -1,47 +1,61 @@
 #include<Arduino.h>
 
 const int pwma=;
-const int an1=;
-const int an2=;
+const int ain1=;
+const int ain2=;
 const int pwmb=;
-const int bn1=;
-const int bn2=;
+const int bin1=;
+const int bin2=;
+
+void forward()
+{
+    digitalWrite(ain1,HIGH);
+    digitalWrite(ain2,LOW);
+    digitalWrite(bin1,HIGH);
+    digitalWrite(bin2,LOW);
+    
+}
+void backward()
+{
+    digitalWrite(ain1,LOW);
+    digitalWrite(ain2,HIGH);
+    digitalWrite(bin1,LOW);
+    digitalWrite(bin2,HIGH);
+    
+}
+void slow_fast()
+{
+    delay(400);
+    for(int i=0;i<256;i++){
+        analogWrite(pwma,i);
+        analogWrite(pwmb,i);
+        delay(10);
+    }
+}
+void fast_slow()
+{
+    delay(400);
+    for(int i=255;i>=0;i--){
+        analogWrite(pwma,i);
+        analogWrite(pwmb,i);
+        delay(10);
+    }
+}
 void setup()
 {
     pinMode(pwma,OUTPUT); 
-    pinMode(an1,OUTPUT);
-    pinMode(an2,OUTPUT);
+    pinMode(ain1,OUTPUT);
+    pinMode(ain2,OUTPUT);
     pinMode(pwmb,OUTPUT);
-    pinMode(bn1,OUTPUT);
-    pinMode(bn2,OUTPUT);
+    pinMode(bin1,OUTPUT);
+    pinMode(bin2,OUTPUT);
 }
 void loop()
 {
-    //forward speed slow to fast
-    digitalWrite(an1,HIGH);
-    digitalWrite(an2,LOW);
-    digitalWrite(bn1,HIGH);
-    digitalWrite(bn2,LOW);
-    for(int i=0;i<256;i++){
-        analogWrite(pwma,i);
-        analogWrite(pwmb,i);
-    }
-    for(int i=255;i>=0;i--){
-        analogWrite(pwma,i);
-        analogWrite(pwmb,i);
-    }
-
-    //backward speed slow to fast
-    digitalWrite(an1,LOW);
-    digitalWrite(an2,HIGH);
-    digitalWrite(bn1,LOW);
-    digitalWrite(bn2,HIGH);
-    for(int i=0;i<256;i++){
-        analogWrite(pwma,i);
-        analogWrite(pwmb,i);
-    }
-    for(int i=255;i>=0;i--){
-        analogWrite(pwma,i);
-        analogWrite(pwmb,i);
-    }
+    forward();
+    slow_fast();
+    fast_slow();
+    backward();
+    slow_fast();
+    fast_slow();
 }
